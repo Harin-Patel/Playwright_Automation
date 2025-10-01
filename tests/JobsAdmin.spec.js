fdf606eba5ac210cf4981936298d5287ad3e51d6
Harin <inx-admin@Harins-mac-mini.local>
2025-10-01 17:48:40 +0530

    Add Playwright tests for editing user details and navigating to user details page
    - Implemented tests for editing user information, including validation of updated details and confirmation of successful updates.
    - Added a test for navigating to a user's details page, verifying the presence of user-specific information and ensuring proper navigation flow.
    - Enhanced user interaction with wait times and toast message validations for better feedback during tests.

---
+++ tests/JobsAdmin.spec.js
@@ -0,0 +1,147 @@
+const{test,expect} = require('@playwright/test')
+
+// Test: Navigate to Jobs Admin page
+test("Navigate to Jobs Admin page", async function({ page }){
+    // Navigate to the homepage
+    await page.goto("http://tgn-frontend-staging-375478166582-us-east-1.s3-website-us-east-1.amazonaws.com/")
+    await page.waitForTimeout(3000)
+
+    // Click on the 'Login' link
+    await page.locator("//a[normalize-space()='Login']").click()
+    await page.waitForTimeout(2000)
+
+    // Enter valid email and password
+    await page.getByPlaceholder("Enter your email").type("admin@jobapp.com")
+    await page.getByPlaceholder("Enter your password").type("admin123")
+
+    // Click the eye icon to view the password
+    await page.locator("//button[@aria-label='Show password']//*[name()='svg']").click()
+    await page.waitForTimeout(1000)
+
+    // Click the Login button
+    await page.locator("//button[normalize-space()='Login']").click()
+    await page.waitForTimeout(5000)
+
+    // Capture and log the toast message
+    const toast = await page.locator("//div[@role='status']").textContent()
+    console.log("The toast message is: " + toast)
+    await page.waitForTimeout(3000)
+
+    // Verify URL to confirm navigation to Dashboard Admin
+    await expect(page).toHaveURL("http://tgn-frontend-staging-375478166582-us-east-1.s3-website-us-east-1.amazonaws.com/admin")
+    //await page.waitForTimeout(2000)
+
+    // Click on the Jobs from the side menu
+    await page.locator("//div[@class='flex flex-col flex-grow bg-white border-r border-accent-200 rounded-r-2xl shadow-2xl']//a[@class='group flex items-center px-4 py-3 text-lg font-semibold rounded-xl transition-all duration-200 text-gray-600 hover:bg-primary-50 hover:text-primary-700 focus:outline-none focus:ring-2 focus:ring-accent-400 focus:ring-offset-2'][normalize-space()='Jobs']").first().click()
+    await page.waitForTimeout(2000)
+
+    // Verify URL to confirm navigation to Jobs Management page
+    await expect(page).toHaveURL("http://tgn-frontend-staging-375478166582-us-east-1.s3-website-us-east-1.amazonaws.com/admin/jobs")
+    await page.waitForTimeout(2000)
+
+    // Capture and log the page title
+    const pageTitle = await page.locator("//h1[normalize-space()='Job Management']").textContent()
+    console.log("The page title is: " + pageTitle)
+    await page.waitForTimeout(2000)
+})
+
+// Test: Add New Job
+test.only("Add New Job", async function({ page }){
+    // Navigate to the homepage
+    await page.goto("http://tgn-frontend-staging-375478166582-us-east-1.s3-website-us-east-1.amazonaws.com/")
+    await page.waitForTimeout(3000)
+
+    // Click on the 'Login' link
+    await page.locator("//a[normalize-space()='Login']").click()
+    await page.waitForTimeout(2000)
+
+    // Enter valid email and password
+    await page.getByPlaceholder("Enter your email").type("admin@jobapp.com")
+    await page.getByPlaceholder("Enter your password").type("admin123")
+
+    //Click the eye icon to view the password
+    await page.locator("//button[@aria-label='Show password']//*[name()='svg']").click()
+    await page.waitForTimeout(1000)
+
+    // Click the Login button
+    await page.locator("//button[normalize-space()='Login']").click()
+    await page.waitForTimeout(3000)
+
+    // Capture and log the toast message
+    const toast = await page.locator("//div[@role='status']").textContent()
+    console.log("The toast message is: " + toast)
+    //await page.waitForTimeout(2000)
+
+    // Verify URL to confirm navigation to Dashboard Admin
+    await expect(page).toHaveURL("http://tgn-frontend-staging-375478166582-us-east-1.s3-website-us-east-1.amazonaws.com/admin")
+    //await page.waitForTimeout(2000)
+
+    // Click on the Jobs from the side menu
+    await page.locator("//div[@class='flex flex-col flex-grow bg-white border-r border-accent-200 rounded-r-2xl shadow-2xl']//a[@class='group flex items-center px-4 py-3 text-lg font-semibold rounded-xl transition-all duration-200 text-gray-600 hover:bg-primary-50 hover:text-primary-700 focus:outline-none focus:ring-2 focus:ring-accent-400 focus:ring-offset-2'][normalize-space()='Jobs']").first().click()
+    //await page.waitForTimeout(2000)
+
+    // Verify URL to confirm navigation to Jobs Management page
+    await expect(page).toHaveURL("http://tgn-frontend-staging-375478166582-us-east-1.s3-website-us-east-1.amazonaws.com/admin/jobs")
+    //await page.waitForTimeout(2000)
+
+    // Capture and log the page title
+    const pageTitle = await page.locator("//h1[normalize-space()='Job Management']").textContent()
+    console.log("The page title is: " + pageTitle)
+    //await page.waitForTimeout(2000)
+
+    // Click on the 'Add Job' button
+    await page.locator("//button[normalize-space()='Add Job']").click()
+    //await page.waitForTimeout(2000)
+
+    // Fill in the job details
+    const facilityTypeCombobox = page.getByPlaceholder("Search facilities...")
+    await facilityTypeCombobox.click()
+    await facilityTypeCombobox.fill("Advanced Care of St Joseph")
+    await page.waitForTimeout(2000)
+    await page.locator("//div[normalize-space()='Advanced Care of St Joseph']").first().click()
+    await page.waitForTimeout(200)
+    
+    const certificationTypeCombobox = page.getByPlaceholder("Search certifications...")
+    await certificationTypeCombobox.click()
+    await certificationTypeCombobox.fill("Assembler")
+    await page.waitForTimeout(1000)
+    await page.locator('div:has-text("Assembler")').first().click()
+    await page.waitForTimeout(2000)
+
+    const specialtyTypeCombobox = page.getByPlaceholder("Search specialties...")
+    await specialtyTypeCombobox.click()
+    await specialtyTypeCombobox.fill("Hospital")
+    await page.waitForTimeout(2000)
+    await page.locator("//div[normalize-space()='Hospital']").first().click()
+    await page.waitForTimeout(2000)
+
+    await page.getByPlaceholder("e.g., 50000").fill("2025")
+    await page.waitForTimeout(2000)
+
+    const stateTypeCombobox = page.getByPlaceholder("Search states...")
+    await stateTypeCombobox.click()
+    await stateTypeCombobox.fill("New York")
+    await page.waitForTimeout(2000)
+    await page.locator("//div[normalize-space()='New York (NY)']").first().click()
+    await page.waitForTimeout(200)
+
+    const shiftTypeCombobox = page.locator("//select[@name='shift']")
+    await shiftTypeCombobox.click()
+    await page.waitForTimeout(2000)
+    await shiftTypeCombobox.selectOption("Night")
+    await page.waitForTimeout(2000)
+
+    const shiftTimeTypeCombobox = page.locator("//select[@name='shiftTime']")
+    await shiftTimeTypeCombobox.click()
+    await page.waitForTimeout(2000)
+    await shiftTimeTypeCombobox.selectOption("H10")
+    await page.waitForTimeout(2000)
+
+    await page.locator("//button[normalize-space()='Create Job']").click()
+    await page.waitForTimeout(3000)
+
+    // Capture and log the toast message
+    const jobToast = await page.locator("//div[@role='status']").textContent()
+    console.log("The toast message is: " + jobToast)
+    await page.waitForTimeout(200)
+})
--- tests/UsersAdmin.spec.js
+++ tests/UsersAdmin.spec.js
@@ -200,3 +200,126 @@ test("Verify Delete user functionality", async function({ page }){
     const userRows = await page.locator("//tbody/tr").count();
     expect(userRows).toBe(0);
 })
+
+// Test: Edit User details
+test("Edit User details", async function({ page }){
+     // Navigate to the homepage
+    await page.goto("http://tgn-frontend-staging-375478166582-us-east-1.s3-website-us-east-1.amazonaws.com/")
+    await page.waitForTimeout(3000)
+
+    // Click on the 'Login' link
+    await page.locator("//a[normalize-space()='Login']").click()
+    await page.waitForTimeout(2000)
+
+    // Enter valid email and password
+    await page.getByPlaceholder("Enter your email").type("admin@jobapp.com")
+    await page.getByPlaceholder("Enter your password").type("admin123")
+
+    //Click the eye icon to view the password
+    await page.locator("//button[@aria-label='Show password']//*[name()='svg']").click()
+    await page.waitForTimeout(1000)
+
+    // Click the Login button
+    await page.locator("//button[normalize-space()='Login']").click()
+    await page.waitForTimeout(5000)
+
+    // Capture and log the toast message
+    const toast = await page.locator("//div[@role='status']").textContent()
+    console.log("The toast message is: " + toast)
+    await page.waitForTimeout(3000)
+
+    // Verify URL to confirm navigation to Dashboard Admin
+    await expect(page).toHaveURL("http://tgn-frontend-staging-375478166582-us-east-1.s3-website-us-east-1.amazonaws.com/admin")
+    await page.waitForTimeout(2000)
+
+    // Navigating to Users tab
+    await page.locator("//div[@class='flex flex-col flex-grow bg-white border-r border-accent-200 rounded-r-2xl shadow-2xl']//a[@class='group flex items-center px-4 py-3 text-lg font-semibold rounded-xl transition-all duration-200 text-gray-600 hover:bg-primary-50 hover:text-primary-700 focus:outline-none focus:ring-2 focus:ring-accent-400 focus:ring-offset-2'][normalize-space()='Users']").first().click()
+    await page.waitForTimeout(2000)
+
+    // Search for the user to be edited
+    await page.locator("//input[@placeholder='Search users...']").fill("Julia")
+    await page.waitForTimeout(2000)
+
+    // Click on the edit icon to edit the user details
+    await page.locator("//tbody/tr[1]/td[5]/div[1]/button[1]//*[name()='svg']").click()
+    await page.waitForTimeout(2000)
+
+    // Update the user details
+    await page.locator("//input[@name='firstName']").fill("")
+    await page.locator("//input[@name='firstName']").type("Julia", {delay:100})
+    await page.locator("//input[@name='lastName']").fill("")
+    await page.locator("//input[@name='lastName']").type("Roberts", {delay:100})
+    await page.locator("//input[@name='email']").fill("")
+    await page.locator("//input[@name='email']").type("julia@mailnesia.com", {delay:100})
+    await page.locator("//input[@id='mobile']").fill("")
+    await page.locator("//input[@id='mobile']").type("9123456780", {delay:100})
+    await page.locator("//button[normalize-space()='Update']").click()
+    await page.waitForTimeout(2000)
+
+    //Verify the toast message after updating user details
+    const toast1 = await page.locator("//div[@role='status']").textContent()
+    console.log("The toast message is: " + toast1)
+    await page.waitForTimeout(2000)
+    
+    // Verify URL to confirm still on Users tab
+    await expect(page).toHaveURL("http://tgn-frontend-staging-375478166582-us-east-1.s3-website-us-east-1.amazonaws.com/admin/users")
+    await page.waitForTimeout(2000)
+})
+
+// Test: Verfiy navigating to the user's details page
+test.only("Verfiy navigating to the user's details page", async function({ page }){
+     // Navigate to the homepage
+    await page.goto("http://tgn-frontend-staging-375478166582-us-east-1.s3-website-us-east-1.amazonaws.com/")
+    await page.waitForTimeout(3000)
+
+    // Click on the 'Login' link
+    await page.locator("//a[normalize-space()='Login']").click()
+    await page.waitForTimeout(2000)
+
+    // Enter valid email and password
+    await page.getByPlaceholder("Enter your email").type("admin@jobapp.com")
+    await page.getByPlaceholder("Enter your password").type("admin123")
+
+    //Click the eye icon to view the password
+    await page.locator("//button[@aria-label='Show password']//*[name()='svg']").click()
+    await page.waitForTimeout(1000)
+
+    // Click the Login button
+    await page.locator("//button[normalize-space()='Login']").click()
+    await page.waitForTimeout(5000)
+
+    // Capture and log the toast message
+    const toast = await page.locator("//div[@role='status']").textContent()
+    console.log("The toast message is: " + toast)
+    await page.waitForTimeout(3000)
+
+    // Verify URL to confirm navigation to Dashboard Admin
+    await expect(page).toHaveURL("http://tgn-frontend-staging-375478166582-us-east-1.s3-website-us-east-1.amazonaws.com/admin")
+    await page.waitForTimeout(2000)
+
+    // Navigating to Users tab
+    await page.locator("//div[@class='flex flex-col flex-grow bg-white border-r border-accent-200 rounded-r-2xl shadow-2xl']//a[@class='group flex items-center px-4 py-3 text-lg font-semibold rounded-xl transition-all duration-200 text-gray-600 hover:bg-primary-50 hover:text-primary-700 focus:outline-none focus:ring-2 focus:ring-accent-400 focus:ring-offset-2'][normalize-space()='Users']").first().click()
+    await page.waitForTimeout(2000)
+
+    // Search for the user to be edited
+    await page.locator("//input[@placeholder='Search users...']").fill("Julia")
+    await page.waitForTimeout(2000)
+
+    // Click on the eye icon to navigate to the user's details page
+    await page.locator("//tbody/tr[1]/td[5]/div[1]/button[3]//*[name()='svg']").click()
+    await page.waitForTimeout(2000)
+
+    // Verify opening of user's details page by checking for the presence of 'User Details' text with the user's name
+    const userDetailsHeader = await page.locator("//h2[normalize-space()='User Details - Julia Roberts']").textContent()
+    console.log("The header text is: " + userDetailsHeader)
+    await expect(page.locator("//h2[normalize-space()='User Details - Julia Roberts']")).toBeVisible()
+    await page.waitForTimeout(2000)
+
+    // Close the user details page and return to Users tab
+    await page.locator("//button[normalize-space()='×']").click()
+    await page.waitForTimeout(2000)
+
+    // Verify URL to confirm still on Users tab
+    await expect(page).toHaveURL("http://tgn-frontend-staging-375478166582-us-east-1.s3-website-us-east-1.amazonaws.com/admin/users")
+    await page.waitForTimeout(2000)
+})
